303c4a38ab52047920349c778f3e368ceb8744af
Med Aduh <im.ramidem@gmail.com>
2018-12-26 00:36:40 +0800

    Better Shipping algorithm

--- Python/Codecademy/CS_Path/03-Flow_Data_and_Iteration/01-Control_Flow/sals_shipping.py
+++ Python/Codecademy/CS_Path/03-Flow_Data_and_Iteration/01-Control_Flow/sals_shipping.py
@@ -1,39 +1,57 @@
+# Ground Shipping Cost
 def ground_shipping_cost(weight):
-    if weight <= 2:
-      return weight * 1.50 + 20.00
-    elif weight > 2 and weight <= 6:
-      return weight * 3.00 + 20.00
-    elif weight > 6 and weight <= 10:
-      return weight * 4.00 + 20.00
-    elif weight > 10:
-      return weight * 4.75 + 20.00
 
+    if weight <= 2:
+        price_per_pound = 1.50
+    elif weight > 2 and weight <= 6:
+        price_per_pound = 3.00
+    elif weight > 6 and weight <= 10:
+        price_per_pound = 4.00
+    else:
+        price_per_pound = 4.75
+
+    return weight * price_per_pound + 20
+
+# Premium Shipping Cost
 premium_shipping_cost = 125.00
 
+# Drone Shipping Cost
 def drone_shipping_cost(weight):
+
     if weight <= 2:
-      return weight * 4.50 + 0.00
+        price_per_pound = 4.50
     elif weight > 2 and weight <= 6:
-      return weight * 9.00 + 0.00
+        price_per_pound = 9.00
     elif weight > 6 and weight <= 10:
-      return weight * 12.00 + 0.00
-    elif weight > 10:
-      return weight * 14.25 + 0.00
+        price_per_pound = 12.00
+    else:
+        price_per_pound = 14.25
 
-def cheapest_shipping(weight):
-  cost = ""
-  drone = drone_shipping_cost(weight)
-  ground = ground_shipping_cost(weight)
-  
-  if ground < drone:
-    cost = "You should ship using ground shipping, it will cost $" + str(ground)
-  else:
-    cost = "You should ship using drone shipping, it will cost $" + str(drone)
+    return weight * price_per_pound
     
-  return cost
 
-print(cheapest_shipping(17))
-print(cheapest_shipping(1.5))
-print(cheapest_shipping(8.4))
+# Return the cheapest shipping cost
+def cheapest_shipping(weight):
+    cost = ""
+    method = ""
+    drone = drone_shipping_cost(weight)
+    ground = ground_shipping_cost(weight)
+  
+    if ground < drone:
+        cost = ground
+        method = "ground"
+    else:
+        cost = drone
+        method = "drone"
+
+    print(
+        "You should ship using %s shipping, it will cost $%s"
+        %(method, cost)
+    
+    )
+    
+cheapest_shipping(17)
+cheapest_shipping(1.5)
+cheapest_shipping(8.4)
   
 
